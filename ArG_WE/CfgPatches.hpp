bbd0131b2c24bb9feec3800b4ee33820f146e8e5
Rodeostar42 <Rodeostar42@users.noreply.github.com>
2018-03-28 02:57:49 +0900

    f

---
+++ ArG_WE/CfgPatches.hpp
@@ -0,0 +1,23 @@
+class CfgPatches
+{
+	class ArG_WE_Weapons
+	{
+
+		requiredAddons[]=
+		{
+
+		};
+		requiredVersion=0.1;
+		units[]={};
+		weapons[]=
+		{
+			
+			"ArG_WE_SVD"
+			
+		};
+	};
+};
+
+
+
+
