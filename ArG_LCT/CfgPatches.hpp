d75bd394f0b24453c6e488afad7379ff2ebdb518
Rodeostar42 <Rodeostar42@users.noreply.github.com>
2018-03-28 02:51:07 +0900

    c

---
+++ ArG_LCT/CfgPatches.hpp
@@ -0,0 +1,22 @@
+class CfgPatches
+{
+	class ArG_LCT_Weapons
+	{
+
+		requiredAddons[]=
+		{
+
+		};
+		requiredVersion=0.1;
+		units[]={};
+		weapons[]=
+		{
+			"ArG_LCT_AKM",
+			"ArG_LCT_VAL"
+		};
+	};
+};
+
+
+
+
