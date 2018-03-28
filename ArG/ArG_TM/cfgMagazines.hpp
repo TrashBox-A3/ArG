7b518c545571d925ef0063b5b8cb1ac645f683e9
Rodeostar42 <Rodeostar42@users.noreply.github.com>
2018-03-28 12:54:42 +0900

    g

---
+++ ArG/ArG_TM/cfgMagazines.hpp
@@ -0,0 +1,147 @@
+class cfgMagazines
+{
+    
+    
+    class rhs_mag_30Rnd_556x45_M855A1_Stanag;
+    class ArG_520Rnd_556x45_BB: rhs_mag_30Rnd_556x45_M855A1_Stanag
+    {
+    	author="Rodeostar42";
+		displayName = "520Rnd 556x45 6mmBB";
+		scope = 2;
+		ammo = ArG_6mm_BB_02; //eg
+		count = 520;
+		initSpeed = 100; // in case it is not in weapon
+		tracersEvery=1;
+		class ItemInfo
+        	{
+            		mass = 1;
+        	}; 
+		picture = "\A3\weapons_F\data\UI\m_30stanag_CA.paa";
+		model = "\rhsusf\addons\rhsusf_weapons\magazines\rhs_stanag_mag";
+		descriptionShort = "Caliber: 6mm BB<br/>Rounds: 520<br/>weight: 2.0g";
+		displayNameMFDFormat = "6mmBB";
+		
+	};
+
+	class ArG_82Rnd_556x45_BB: rhs_mag_30Rnd_556x45_M855A1_Stanag
+    {
+    	author="Rodeostar42";
+		displayName = "82Rnd 556x45 6mmBB";
+		scope = 2;
+		ammo = ArG_6mm_BB_02; //eg
+		count = 82;
+		initSpeed = 100; // in case it is not in weapon
+		tracersEvery=1;
+		class ItemInfo
+        	{
+            		mass = 1;
+        	}; 
+		picture = "\A3\weapons_F\data\UI\m_30stanag_CA.paa";
+		model = "\rhsusf\addons\rhsusf_weapons\magazines\rhs_stanag_mag";
+		descriptionShort = "Caliber: 6mm BB<br/>Rounds: 82<br/>weight: 2.0g";
+		displayNameMFDFormat = "6mmBB";
+		
+	};
+
+	class ArG_35Rnd_556x45_NWS: rhs_mag_30Rnd_556x45_M855A1_Stanag
+    {
+    	author="Rodeostar42";
+		displayName = "35Rnd NWS 6mmBB";
+		scope = 2;
+		ammo = ArG_6mm_BB_02; //eg
+		count = 35;
+		initSpeed = 100; // in case it is not in weapon
+		tracersEvery=1;
+		class ItemInfo
+        	{
+            		mass = 1;
+        	}; 
+		picture = "\A3\weapons_F\data\UI\m_30stanag_CA.paa";
+		model = "\rhsusf\addons\rhsusf_weapons\magazines\rhs_stanag_mag";
+		descriptionShort = "Caliber: 6mm BB<br/>Rounds: 35<br/>weight: 2.0g";
+		displayNameMFDFormat = "6mmBB";
+		
+	};
+
+	class ArG_30Rnd_556x45_TOP: rhs_mag_30Rnd_556x45_M855A1_Stanag
+    {
+    	author="Rodeostar42";
+		displayName = "30Rnd Top 6mmBB";
+		scope = 2;
+		ammo = ArG_6mm_BB_02_TOP; //eg
+		count = 30;
+		initSpeed = 100; // in case it is not in weapon
+		tracersEvery=1;
+		class ItemInfo
+        	{
+            		mass = 1;
+        	}; 
+		picture = "\A3\weapons_F\data\UI\m_30stanag_CA.paa";
+		model = "\rhsusf\addons\rhsusf_weapons\magazines\rhs_stanag_mag";
+		descriptionShort = "Caliber: 6mm BB<br/>Rounds: 30<br/>weight: 2.0g";
+		displayNameMFDFormat = "6mmBB";
+		
+	};
+
+	class ArG_25Rnd_9mm: rhs_mag_30Rnd_556x45_M855A1_Stanag
+    {
+    	author="Rodeostar42";
+		displayName = "25Rnd 6mmBB";
+		scope = 2;
+		ammo = ArG_6mm_BB_02; //eg
+		count = 25;
+		initSpeed = 100; // in case it is not in weapon
+		tracersEvery=1;
+		class ItemInfo
+        	{
+            		mass = 1;
+        	}; 
+		picture = "\rhsusf\addons\rhsusf_weapons2\glock17g4\data\rhs_mag2_glock17g4_ca.paa";
+		model = "\A3\weapons_F\ammo\mag_univ.p3d";
+		descriptionShort = "Caliber: 6mm BB<br/>Rounds: 25<br/>weight: 2.0g";
+		displayNameMFDFormat = "6mmBB";
+		
+	};
+
+
+    class rhsusf_20Rnd_762x51_m118_special_Mag;
+	class ArG_90Rnd_762x51_BB: rhsusf_20Rnd_762x51_m118_special_Mag
+    {
+    	author="Rodeostar42";
+		displayName = "90Rnd 6mmBB";
+		scope = 2;
+		ammo = ArG_6mm_BB_02; //eg
+		count = 90;
+		initSpeed = 100; // in case it is not in weapon
+		tracersEvery=1;
+		class ItemInfo
+        	{
+            		mass = 1;
+        	}; 
+		picture = "\A3\weapons_f\data\UI\M_20Rnd_762x51_CA.paa";
+		model = "\rhsusf\addons\rhsusf_weapons\magazines\rhs_m14_mag";
+		descriptionShort = "Caliber: 6mm BB<br/>Rounds: 90<br/>weight: 2.0g";
+		displayNameMFDFormat = "6mmBB";
+		
+	};
+
+	class ArG_540Rnd_762x51_BB: rhsusf_20Rnd_762x51_m118_special_Mag
+    {
+    	author="Rodeostar42";
+		displayName = "540Rnd 6mmBB";
+		scope = 2;
+		ammo = ArG_6mm_BB_02; //eg
+		count = 540;
+		initSpeed = 100; // in case it is not in weapon
+		tracersEvery=1;
+		class ItemInfo
+        	{
+            		mass = 1;
+        	}; 
+		picture = "\A3\weapons_f\data\UI\M_20Rnd_762x51_CA.paa";
+		model = "\rhsusf\addons\rhsusf_weapons\magazines\rhs_m14_mag";
+		descriptionShort = "Caliber: 6mm BB<br/>Rounds: 540<br/>weight: 2.0g";
+		displayNameMFDFormat = "6mmBB";
+		
+	};
+};
