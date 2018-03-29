a51b6e53c3de57a32d0d2855351917fb8921e8d9
Rodeostar42 <rodeostar42@gmail.com>
2018-03-29 19:07:21 +0900

    RecoileChange

--- ArG/ArG_Core/cfgRecoils.hpp
+++ ArG/ArG_Core/cfgRecoils.hpp
@@ -3,6 +3,7 @@ class cfgRecoils
  class GBRecoil
  {              //  x     y     a     b
   muzzleOuter[]	= { 0.2,  0.5,  0.2,  0.1 };
+  muzzleInner[] = {0,0,0.1,0.1};
   kickBack[]	= { 0.03, 0.06 };
   permanent	= 0.1;
   temporary	= 0.01;
@@ -11,6 +12,7 @@ class cfgRecoils
  class EGRecoil
  {              //  x     y     a     b
   muzzleOuter[]	= { 0.1,  0.1,  0.1,  0.1 };
+  muzzleInner[] = {0,0,0.1,0.1};
   kickBack[]	= { 0.01, 0.01 };
   permanent	= 0.1;
   temporary	= 0.01;
@@ -19,6 +21,7 @@ class cfgRecoils
  class NextEGRecoil
  {              //  x     y     a     b
   muzzleOuter[]	= { 0.1,  0.2,  0.1,  0.1 };
+  muzzleInner[] = {0,0,0.1,0.1};
   kickBack[]	= { 0.01, 0.03 };
   permanent	= 0.1;
   temporary	= 0.01;
@@ -27,6 +30,7 @@ class cfgRecoils
  class HGRecoil
  {              //  x     y     a     b
   muzzleOuter[]	= { 0.3,  0.5,  0.1,  0.1 };
+  muzzleInner[] = {0,0,0.1,0.1};
   kickBack[]	= { 0.01, 0.03 };
   permanent	= 0.1;
   temporary	= 0.01;
