336c43d0c36f6b77902a6990079023140c745f05
Rodeostar42 <rodeostar42@gmail.com>
2018-03-31 09:22:38 +0900

    fdfdfdfdf

--- ArG/ArG_Core/ArG_AllinOne_ItemBox.hpp
+++ ArG/ArG_Core/ArG_AllinOne_ItemBox.hpp
@@ -1,6 +1,6 @@
 
-class Box_NATO_Equip_F;
-    class ArG_AllinOne_Weapons_Box : Box_NATO_Equip_F
+class Box_NATO_AmmoVeh_F;
+    class ArG_AllinOne_Weapons_Box : Box_NATO_AmmoVeh_F
     {
         scope = 2;
         
@@ -49,7 +49,18 @@ class Box_NATO_Equip_F;
             item10(VFC_30Rnd_556x45_M4_HK416_GBB);
             item10(ArG_WE_SVD);
             item10(ArG_20Rnd_762x54_BB);
-            
+            item10(acc_flashlight);
+            item10(optic_Hamr);
+            item10(bipod_01_F_blk);
+            item10(optic_Holosight);
+            item10(optic_Yorris);
+            item10(optic_DMS);
+            item10(optic_SOS);
+            item10(optic_MRCO);
+            item10(optic_LRPS);
+            item10(optic_Aco);
+            item10(acc_pointer_IR);
+            item10(muzzle_snds_H);
             
         };
     };
