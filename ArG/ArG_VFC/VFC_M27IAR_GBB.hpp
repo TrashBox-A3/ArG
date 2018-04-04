cc38d306d117a4fd9dd28cab62d843f52401d6c5
Rodeostar42 <rodeostar42@gmail.com>
2018-04-04 18:50:23 +0900

    sasasas

--- ArG/ArG_VFC/VFC_M27IAR_GBB.hpp
+++ ArG/ArG_VFC/VFC_M27IAR_GBB.hpp
@@ -9,7 +9,6 @@ class ArG_VFC_M27IAR_GBB: rhs_weap_m27iar
 	picture = "\rhsusf\addons\rhsusf_weapons2\icons\rhs_m27iar_icon_ca.paa";
 	model = "\rhsusf\addons\rhsusf_weapons2\M27\rhs_m27iar.p3d";
 	descriptionShort = "VFC M27 IAR GBB <br/>VFC";
-	handAnim[] = {"OFP2_ManSkeleton","\rhsusf\addons\rhsusf_c_weapons\anims\rhs_hand_m16a4_stock.rtm"};
 
 	magazines[] = {
 
@@ -24,40 +23,5 @@ class ArG_VFC_M27IAR_GBB: rhs_weap_m27iar
 	inertia = 0.4;
 	dexterity = 1.6;
 	initSpeed = 250; // muzzle velocity
-
-	discreteDistance[]={200,300,400,500,600};
-	discreteDistanceCameraPoint[]=
-	{
-		"eye",
-		"eye_300",
-		"eye_400",
-		"eye_500",
-		"eye_600"
-	};
-	discreteDistanceInitIndex=0;
-
-	modes[] =
-	{
-		"Single",
-		"Single_AI",
-		"FullAuto",
-		"single_medium_optics1",
-		"single_far_optics2"
-	};
-
-	class FullAuto : FullAuto
-	{
-		reloadTime = 0.075;
-		dispersion=0.0018;
-		minRange = 2;
-		minRangeProbab = 0.9;
-		midRange = 30;
-		midRangeProbab = 0.7;
-		maxRange = 100;
-		maxRangeProbab = 0.3;
-		aiRateOfFire= 1e-006;
-	};
-
-	
 	baseWeapon="ArG_VFC_M27IAR_GBB";
 };
