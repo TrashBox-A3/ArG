d70af6273be877b8abc9dd632f7c6c3340ba79a8
Rodeostar42 <rodeostar42@gmail.com>
2018-04-04 19:01:50 +0900

    sasasassa

--- ArG/ArG_VFC/VFC_ItemBox.hpp
+++ ArG/ArG_VFC/VFC_ItemBox.hpp
@@ -2,18 +2,19 @@ class Box_NATO_Equip_F;
     class VFC_Weapons_Box : Box_NATO_Equip_F
     {
         scope = 2;
-        
+
         displayName = "VFC Weapons Box";
         DLC = "";
         editorSubcategory = "ArG_Weapons_Box";
         editorCategory = "ArG_Objects";
-        
+
         class TransportItems
         {
             item10(ArG_VFC_HK416A5_GBB_BLK);
             item10(ArG_VFC_HK416A5_GBB_SND);
+            item10(ArG_VFC_M27IAR_GBB);
             item10(VFC_30Rnd_556x45_M4_HK416_GBB);
             item10(G_EyeProtectors_F);
-            
+
         };
     };
