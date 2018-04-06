0443001488f0bba34d7ab71180783bca45ad127d
Rodeostar42 <rodeostar42@gmail.com>
2018-04-06 22:06:25 +0900

    sasasasa

--- ArG/ArG_Core/ArG_ItemBox.hpp
+++ ArG/ArG_Core/ArG_ItemBox.hpp
@@ -25,6 +25,18 @@ class Box_NATO_Equip_F;
             item10(ArG_200Rnd_65x39_cased_Box_BB_02);
             item10(ArG_200Rnd_65x39_cased_Box_BB_025);
             item10(G_Combat);
+            item10(acc_flashlight);
+            item10(optic_Hamr);
+            item10(bipod_01_F_blk);
+            item10(optic_Holosight);
+            item10(optic_Yorris);
+            item10(optic_DMS);
+            item10(optic_SOS);
+            item10(optic_MRCO);
+            item10(optic_LRPS);
+            item10(optic_Aco);
+            item10(acc_pointer_IR);
+            item10(muzzle_snds_H);
 
         };
     };
