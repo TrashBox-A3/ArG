fae76b1560e5cea5173dff627728ba318418aa98
Rodeostar42 <rodeostar42@gmail.com>
2018-04-17 21:30:00 +0900

    sasasas

--- ArG/ArG_Core/ArG_ItemBox.hpp
+++ ArG/ArG_Core/ArG_ItemBox.hpp
@@ -38,26 +38,7 @@ class Box_NATO_Equip_F;
             item10(optic_Aco);
             item10(acc_pointer_IR);
             item10(muzzle_snds_H);
-            ///A&K///
-            item10(ArG_AK_PKM);
-            item10(ArG_AK_SR25K);
-            item10(ArG_5000Rnd_762x54_BB);
-            item10(ArG_90Rnd_762x51_BB);
-            ///GHK///
-            item10(ArG_GHK_AKM);
-            item10(ArG_40Rnd_762x39_BB);
-            ///KSC///
-            item10(ArG_KSC_PM);
-            item10(ArG_10Rnd_9x18_BB);
-            item10(ArG_KSC_AK74M);
-            item10(ArG_600Rnd_545x39_BB);
-            item10(ArG_70Rnd_545x39_BB);
-            ///LCT///
-            item10(ArG_LCT_AKM);
-            item10(ArG_600Rnd_762x39_BB);
-            item10(ArG_70Rnd_762x39_BB);
-            item10(ArG_LCT_VAL);
-            item10(ArG_50Rnd_9x39_BB);
+            
             ///TM///
             item10(ArG_TM_HK416_DEV);
             item10(ArG_520Rnd_556x45_BB);
@@ -73,13 +54,6 @@ class Box_NATO_Equip_F;
             item10(ArG_25Rnd_9mm);
             item10(ArG_TM_P226R);
             item10(ArG_TM_M92F);
-            ///VFC///
-            item10(ArG_VFC_HK416A5_GBB_BLK);
-            item10(ArG_VFC_HK416A5_GBB_SND);
-            item10(ArG_VFC_M27IAR_GBB);
-            item10(VFC_30Rnd_556x45_M4_HK416_GBB);
-            ///WE///
-            item10(ArG_WE_SVD);
-            item10(ArG_20Rnd_762x54_BB);
+
         };
     };
