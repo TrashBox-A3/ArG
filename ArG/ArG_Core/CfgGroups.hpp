548ab306aca9c2876c4163b79aa65f5e6ce8f765
Rodeostar42 <rodeostar42@gmail.com>
2018-04-17 20:47:31 +0900

    wdww

--- ArG/ArG_Core/CfgGroups.hpp
+++ ArG/ArG_Core/CfgGroups.hpp
@@ -47,6 +47,7 @@ class CfgGroups {
                  };
               };
            };
+         };
 
 
            class EAST {
@@ -97,6 +98,7 @@ class CfgGroups {
                           };
                        };
                     };
+                  };
 
 
          };
