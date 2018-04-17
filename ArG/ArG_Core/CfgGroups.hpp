0b115f489ce71cc8470df4cc7c07baffcc9f21ba
Rodeostar42 <rodeostar42@gmail.com>
2018-04-17 20:32:11 +0900

    dwdwdwd

--- ArG/ArG_Core/CfgGroups.hpp
+++ ArG/ArG_Core/CfgGroups.hpp
@@ -15,7 +15,7 @@ class CfgGroups {
                 class ArG_Group_1 {
                     name = "4 Frends";
                     side = 1;
-                    faction = "ArG_Frends_F";
+                    faction = "ArG_Core";
                     icon = "\A3\ui_f\data\map\markers\nato\b_recon.paa";
                     rarityGroup = 0.5;
 
@@ -46,6 +46,54 @@ class CfgGroups {
                  };
               };
            };
+
+
+           class EAST {
+
+                 class ArG_Frends_F {
+                     name = "ArG Game Frends";
+
+                     class Infantry {
+                         name = "Infantry";
+
+                     };
+
+
+
+                         class ArG_Group_2 {
+                             name = "4 Frends";
+                             side = 1;
+                             faction = "ArG_Core";
+                             icon = "\A3\ui_f\data\map\markers\nato\b_recon.paa";
+                             rarityGroup = 0.5;
+
+                             class Unit4 {
+                                 position[] = { 0 , 0 , 0 };
+                                 rank = "LIEUTENANT";
+                                 side = 1;
+                                 vehicle = "ArG_Gamer_5";
+                             };
+                             class Unit5 {
+                                 position[] = { 5 , -5 , 0 };
+                                 rank = "SERGEANT";
+                                 side = 1;
+                                 vehicle = "ArG_Gamer_6";
+                             };
+                             class Unit6 {
+                                 position[] = { -5 , -5 , 0 };
+                                 rank = "SERGEANT";
+                                 side = 1;
+                                 vehicle = "ArG_Gamer_7";
+                             };
+                             class Unit7 {
+                                 position[] = { 10 , -10 , 0 };
+                                 rank = "SERGEANT";
+                                 side = 1;
+                                 vehicle = "ArG_Gamer_8";
+                             };
+                          };
+                       };
+                    };
 
 
          };
