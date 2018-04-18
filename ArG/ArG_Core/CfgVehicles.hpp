e9d49d00c0afbc517ff698f042c8d5c12ba80c96
Rodeostar42 <rodeostar42@gmail.com>
2018-04-18 19:52:42 +0900

    yghghg

--- ArG/ArG_Core/CfgVehicles.hpp
+++ ArG/ArG_Core/CfgVehicles.hpp
@@ -539,6 +539,13 @@ class  O_Soldier_base_F;
         hiddenSelections[]={};
         hiddenSelectionsTextures[] = {};
 
+          class eventHandlers
+	{
+		init = "buzz = this addAction ['<t color=''#FF4500''>Buzzer Switch','this say3D 'Switch', '0 = [0] execVM 'BuzzerBox.sqf''];";
+
+
+	};
+
     };
 
     //Speaker//
