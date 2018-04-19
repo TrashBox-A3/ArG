7379490e7621d0283819ec94cbeb633240261434
Rodeostar42 <rodeostar42@gmail.com>
2018-04-19 22:12:01 +0900

    xzxzxzxzxz

--- ArG/ArG_Core/CfgAmmo.hpp
+++ ArG/ArG_Core/CfgAmmo.hpp
@@ -99,4 +99,24 @@ class CfgAmmo
         coefGravity = 1.2; //default value
 	};
 
+	class ArG_20mm_Test : BulletBase
+	{
+		author="Rodeostar42";
+		typicalSpeed=200; // speed at 100% hit (m/s)
+		caliber=0.80000001;
+		hit = 0.01;
+		airFriction = -0.050;
+		sideAirFriction = 0.9;
+		cartridge = "";
+		supersonicCrackNear[] = {"", 0, 0};
+		supersonicCrackFar[] = {"", 0, 0};
+		suppressionRadiusBulletClose = -1;
+		model = "\ArG_Core\Data\BB\6mm";
+		tracerScale=1.5;
+        tracerStartTime = 0.01; // seconds, -1 disables tracer
+        tracerEndTime = 3; // seconds
+        nvgOnly = false; // tracer visible only by NVG
+        coefGravity = 1.2; //default value
+	};
+
 };
