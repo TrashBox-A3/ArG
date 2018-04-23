d7deb4019fd105947cedf9b5d4370df33ce7f94e
Rodeostar42 <rodeosta42@gmail.com>
2018-04-23 12:03:30 +0900

    dwwdw

--- ArG/ArG_Core/CfgAmmo.hpp
+++ ArG/ArG_Core/CfgAmmo.hpp
@@ -7,7 +7,7 @@ class CfgAmmo
 		author="Rodeostar42";
 		typicalSpeed=300; // speed at 100% hit (m/s)
 		caliber=0.80000001;
-		hit = 0.1;
+		hit = 1;
 		airFriction = -0.020;
 		cartridge = "";
 		supersonicCrackNear[] = {"", 0, 0};
