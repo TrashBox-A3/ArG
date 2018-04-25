3f29a1ccd71f0344ba82b881e8636e44bb351449
Rodeostar42 <rodeosta42@gmail.com>
2018-04-25 16:17:38 +0900

    vdvdvdv

--- ArG/ArG_Core/CfgFunctions.hpp
+++ ArG/ArG_Core/CfgFunctions.hpp
@@ -6,8 +6,8 @@ class ArG
 {
 		file = "\ArG_Core\functions";
 		class init { postInit = 1; };
-		class HitCall { postInit = 1; };
-		class Ai_HitCall { postInit = 1; };
+		class HitCall {};
+		class Ai_HitCall {};
 
 	};
 };
