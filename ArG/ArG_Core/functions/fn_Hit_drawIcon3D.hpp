3539937b347b17f19fba2580dcda5f6cd251d687
Rodeostar42 <rodeosta42@gmail.com>
2018-04-25 17:23:13 +0900

    dwdwdw

---
+++ ArG/ArG_Core/functions/fn_Hit_drawIcon3D.hpp
@@ -0,0 +1,18 @@
+params ["_text"];
+
+onEachFrame {
+
+    _HitPos = getPosATL _text;
+    drawIcon3D [
+        "",
+        [153,0,0,0.5],
+        [_HitPos select 0,_HitPos select 1,2.3],
+        5,
+        5,
+        direction _text,
+        "HIT!",
+        0,
+        0.08,
+        "TahomaB"
+        ];
+};
