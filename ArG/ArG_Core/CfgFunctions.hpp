273c8ce0a49707fbb951422a95d01309ee17eaae
Rodeostar42 <rodeosta42@gmail.com>
2018-04-25 17:02:12 +0900

    dwdwddwdw

--- ArG/ArG_Core/CfgFunctions.hpp
+++ ArG/ArG_Core/CfgFunctions.hpp
@@ -6,10 +6,16 @@ tag = "ArG";
 class ArG_Func
 {
 
+	class State
+  {
+	file = "\ArG_Core\functions";
+	class init { postInit = 1; };
+
+  };
+
 	class Script
 	{
 		file = "\ArG_Core\functions";
-		class init {};
 		class HitCall {};
 		class Ai_HitCall {};
 		class BuzzerBox {};
