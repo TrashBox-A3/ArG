bccd8e383ada697cc55c8a2ac82adb55dfdd1d99
Rodeostar42 <rodeosta42@gmail.com>
2018-05-22 05:21:04 +0900

    dwdwdwdw

--- ArG/ArG_Core/M24.hpp
+++ ArG/ArG_Core/M24.hpp
@@ -1,18 +1,12 @@
+///MK200///
 
-  class arifle_MX_F;
+class LMG_Mk200_F;
+class ArG_M24: LMG_Mk200_F
+{
+  author="Rodeostar42";
 
-  class ArG_M24: arifle_MX_F
-  {
-      author="Rodeostar42";
-
-      scope=2;
-      displayName="ArG M24";
-      model="\ArG_Core\Data\Weapon\M24\JSDF_M24R.p3d";
-      baseWeapon="ArG_M24";
-      picture = "\rhsafrf\addons\rhs_weapons2\icons\rhs_aks74u_inventory.paa";
-      UiPicture = "\A3\weapons_f\data\UI\icon_regular_CA.paa";
-      descriptionShort = "AKS74U<br/>Tokyo MARUI";
-
+  baseWeapon="ArG_M24";
+  scope=2;
   magazines[] = {
 
       "ArG_30Rnd_556x45_BB_02",
@@ -24,24 +18,50 @@
   flash = "";
   flashSize = 0;
   fireLightIntensity=0;
-
-  recoil="EGRecoil";
-  maxZeroing=200;
   discreteDistanceInitIndex=0;
   maxRecoilSway=0.0125;
   swayDecaySpeed=1.25;
   soundBullet[]={"emptySound",1};
-  alternativeFire="Zasleh2";
+  reloadAction="GestureReloadM200";
+  recoil="EGRecoil";
+  maxZeroing=200;
+  handAnim[]=
+  {
+    "OFP2_ManSkeleton",
+    "\A3\weapons_f\Machineguns\M200\data\Anim\M200.rtm"
+  };
+
+  model="\ArG_Core\Data\Weapon\M24\JSDF_M24R.p3d";
+  displayName="ArG M24";
+
+  picture="\A3\weapons_F\Machineguns\M200\Data\UI\gear_m200_X_CA.paa";
+  UiPicture="\A3\weapons_f\data\UI\icon_mg_CA.paa";
+
+  descriptionShort="Replace MK200 to Airsoft Gun";
+
+  hiddenSelections[]=
+  {
+    "camo",
+    "camo1",
+    "camo2"
+  };
+  hiddenSelectionsTextures[]=
+  {
+    "\A3\Weapons_F\Machineguns\M200\data\1st_person_co",
+    "\A3\Weapons_F\Machineguns\M200\data\body_co",
+    "\A3\Weapons_F\Machineguns\M200\data\grip_co"
+  };
 
   modes[]=
   {
-      "Single",
-      "FullAuto",
-      "fullauto_medium",
-      "single_medium_optics1",
-      "single_far_optics2"
+    "manual",
+    "close",
+    "short",
+    "medium",
+    "far_optic1",
+    "far_optic2"
   };
-  class Single: Mode_SemiAuto
+  class manual: Mode_FullAuto
   {
       reloadTime=0.096000001;
 
@@ -49,11 +69,11 @@
       flashSize = 0;
       fireLightIntensity=0;
       dispersion=0.00086999999;
-      minRange=0;
+      minRange=2;
       minRangeProbab=0.5;
-      midRange=40;
+      midRange=200;
       midRangeProbab=0.69999999;
-      maxRange=80;
+      maxRange=400;
       maxRangeProbab=0.30000001;
       drySound[] = {"ArG_Core\Sound\den_tan01.wss",4, 1,15};
       class StandardSound
@@ -69,7 +89,7 @@
       weaponSoundEffect = "DefaultRifle";
   };
   };
-  class FullAuto: Mode_FullAuto
+  class close: manual
   {
       reloadTime=0.096000001;
       dispersion=0.001;
@@ -79,9 +99,9 @@
       fireLightIntensity=0;
       minRange=0;
       minRangeProbab=0.89999998;
-      midRange=40;
+      midRange=15;
       midRangeProbab=0.69999999;
-      maxRange=80;
+      maxRange=30;
       maxRangeProbab=0.1;
       aiRateOfFire=1e-006;
       drySound[] = {"ArG_Core\Sound\den_tan01.wss",4, 1,15};
@@ -98,16 +118,16 @@
       weaponSoundEffect = "DefaultRifle";
   };
   };
-  class fullauto_medium: FullAuto
+  class short: close
   {
       showToPlayer=0;
       burst=3;
       aiBurstTerminable=1;
-      minRange=0;
+      minRange=2;
       minRangeProbab=0.5;
-      midRange=40;
+      midRange=75;
       midRangeProbab=0.69999999;
-      maxRange=80;
+      maxRange=150;
       maxRangeProbab=0.050000001;
       aiRateOfFire=2;
       aiRateOfFireDistance=200;
@@ -128,15 +148,15 @@
       weaponSoundEffect = "DefaultRifle";
   };
   };
-  class single_medium_optics1: Single
+  class medium: close
   {
       requiredOpticType=1;
       showToPlayer=0;
-      minRange=0;
+      minRange=2;
       minRangeProbab=0.2;
-      midRange=40;
+      midRange=450;
       midRangeProbab=0.69999999;
-      maxRange=80;
+      maxRange=600;
       maxRangeProbab=0.2;
       aiRateOfFire=6;
       aiRateOfFireDistance=600;
@@ -157,15 +177,15 @@
       weaponSoundEffect = "DefaultRifle";
   };
   };
-  class single_far_optics2: single_medium_optics1
+  class far_optic1: medium
   {
       requiredOpticType=2;
       showToPlayer=0;
-      minRange=0;
+      minRange=100;
       minRangeProbab=0.1;
-      midRange=40;
+      midRange=500;
       midRangeProbab=0.60000002;
-      maxRange=80;
+      maxRange=700;
       maxRangeProbab=0.050000001;
       aiRateOfFire=8;
       aiRateOfFireDistance=700;
@@ -186,4 +206,34 @@
       weaponSoundEffect = "DefaultRifle";
   };
   };
-};
+
+  class far_optic2: far_optic1
+  {
+      requiredOpticType=2;
+      showToPlayer=0;
+      minRange=100;
+      minRangeProbab=0.1;
+      midRange=500;
+      midRangeProbab=0.60000002;
+      maxRange=700;
+      maxRangeProbab=0.050000001;
+      aiRateOfFire=8;
+      aiRateOfFireDistance=700;
+      flash = "";
+      flashSize = 0;
+      fireLightIntensity=0;
+      drySound[] = {"ArG_Core\Sound\den_tan01.wss",4, 1,15};
+      class StandardSound
+  {
+      begin1[] = {"ArG_Core\Sound\den_tan01.wss",4, 1,15};
+      soundBegin[] = {"begin1",1};
+      weaponSoundEffect = "DefaultRifle";
+  };
+      class SilencedSound
+  {
+      begin1[] = {"ArG_Core\Sound\den_tan01.wss",3, 1,15};
+      soundBegin[] = {"begin1",1};
+      weaponSoundEffect = "DefaultRifle";
+  };
+  };
+  };
