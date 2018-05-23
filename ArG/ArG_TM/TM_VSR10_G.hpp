14359459f27c3f5c4a04d0a1dbc6feb0c1b47a14
Rodeostar42 <rodeosta42@gmail.com>
2018-05-24 08:51:09 +0900

    dwdwdwdwdd

--- ArG/ArG_TM/TM_VSR10_G.hpp
+++ ArG/ArG_TM/TM_VSR10_G.hpp
@@ -30,3 +30,101 @@ class ArG_TM_VSR10_G: rhs_weap_m24sws
   maxRecoilSway=0.0125;
   swayDecaySpeed=1.25;
   soundBullet[]={"emptySound",1};
+
+  modes[]=
+  {
+      "Single",
+      "single_medium_optics1",
+      "single_far_optics2"
+  };
+  class Single: Mode_SemiAuto
+  {
+      reloadTime=0.096000001;
+
+      flash = "";
+      flashSize = 0;
+      fireLightIntensity=0;
+      dispersion=0.00086999999;
+      minRange=2;
+      minRangeProbab=0.5;
+      midRange=200;
+      midRangeProbab=0.69999999;
+      maxRange=400;
+      maxRangeProbab=0.30000001;
+      drySound[] = {"ArG_TM\Sound\SCAR_H.wss",4, 1,15};
+      class StandardSound
+  {
+      begin1[] = {"ArG_TM\Sound\SCAR_H.wss",4, 1,15};
+      soundBegin[] = {"begin1",1};
+      weaponSoundEffect = "DefaultRifle";
+  };
+      class SilencedSound
+  {
+      begin1[] = {"ArG_TM\Sound\SCAR_H.wss",3, 1,15};
+      soundBegin[] = {"begin1",1};
+      weaponSoundEffect = "DefaultRifle";
+  };
+  };
+  
+  class single_medium_optics1: Single
+  {
+      requiredOpticType=1;
+      showToPlayer=0;
+      minRange=2;
+      minRangeProbab=0.2;
+      midRange=450;
+      midRangeProbab=0.69999999;
+      maxRange=600;
+      maxRangeProbab=0.2;
+      aiRateOfFire=6;
+      aiRateOfFireDistance=600;
+      flash = "";
+      flashSize = 0;
+      fireLightIntensity=0;
+      drySound[] = {"ArG_TM\Sound\SCAR_H.wss",4, 1,15};
+      class StandardSound
+  {
+      begin1[] = {"ArG_TM\Sound\SCAR_H.wss",4, 1,15};
+      soundBegin[] = {"begin1",1};
+      weaponSoundEffect = "DefaultRifle";
+  };
+      class SilencedSound
+  {
+      begin1[] = {"ArG_TM\Sound\SCAR_H.wss",3, 1,15};
+      soundBegin[] = {"begin1",1};
+      weaponSoundEffect = "DefaultRifle";
+  };
+  };
+  class single_far_optics2: single_medium_optics1
+  {
+      requiredOpticType=2;
+      showToPlayer=0;
+      minRange=100;
+      minRangeProbab=0.1;
+      midRange=500;
+      midRangeProbab=0.60000002;
+      maxRange=700;
+      maxRangeProbab=0.050000001;
+      aiRateOfFire=8;
+      aiRateOfFireDistance=700;
+      flash = "";
+      flashSize = 0;
+      fireLightIntensity=0;
+      drySound[] = {"ArG_TM\Sound\SCAR_H.wss",4, 1,15};
+      class StandardSound
+  {
+      begin1[] = {"ArG_TM\Sound\SCAR_H.wss",4, 1,15};
+      soundBegin[] = {"begin1",1};
+      weaponSoundEffect = "DefaultRifle";
+  };
+      class SilencedSound
+  {
+      begin1[] = {"ArG_TM\Sound\SCAR_H.wss",3, 1,15};
+      soundBegin[] = {"begin1",1};
+      weaponSoundEffect = "DefaultRifle";
+  };
+  };
+
+
+
+};
