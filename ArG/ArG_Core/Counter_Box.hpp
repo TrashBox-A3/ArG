df43d9bbd5e717e08d87698356ef3a580b52d640
Rodeostar42 <rodeosta42@gmail.com>
2018-05-26 08:55:57 +0900

    dwdww

--- ArG/ArG_Core/Counter_Box.hpp
+++ ArG/ArG_Core/Counter_Box.hpp
@@ -18,15 +18,22 @@ class  Land_Laptop_unfolded_F;
 {
 class Action
 {
-  "this say3D 'Switch',[this,'Time Counter',
-  '\a3\ui_f\data\IGUI\Cfg\holdactions\holdAction_connect_ca.paa',
-  '\a3\ui_f\data\IGUI\Cfg\holdactions\holdAction_hack_ca.paa',
-  '_this distance _target < 3',
-  '_caller distance _target < 3',
-  {},
-  {},
-  {[]execVM  'ArG_Core\functions\fn_BuzzerBox.sqf'},
-  {},[],2,0,true,false] remoteExec ['BIS_fnc_holdActionAdd', [0,2] select isDedicated, this]";
+  [this,"Time Counter",
+"\a3\ui_f\data\IGUI\Cfg\holdactions\holdAction_connect_ca.paa",
+"\a3\ui_f\data\IGUI\Cfg\holdactions\holdAction_hack_ca.paa",
+
+"_this distance _target < 3",
+"_caller distance _target < 3",
+{},
+{},
+{ []execVM  "ArG_Core\functions\fn_BuzzerBox.sqf" },
+{},
+[],
+2,
+0,
+false,
+false
+] remoteExec ["BIS_fnc_holdActionAdd", [0,2] select isDedicated, this];
 };
 };
 
