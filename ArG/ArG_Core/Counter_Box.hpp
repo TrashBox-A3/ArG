8fd48a3f053858b75dac3d17d98e4ec9125da754
Rodeostar42 <rodeosta42@gmail.com>
2018-05-26 08:27:17 +0900

    dsdsdsds

--- ArG/ArG_Core/Counter_Box.hpp
+++ ArG/ArG_Core/Counter_Box.hpp
@@ -28,19 +28,13 @@ hideOnUse = 0;
 onlyForPlayer = 1;
 shortcut = "reloadMagazine";
 condition = "true";
-statement = "[this,'Time Counter',
+statement = "[this,"Time Counter",
 '\a3\ui_f\data\IGUI\Cfg\holdactions\holdAction_connect_ca.paa',
 '\a3\ui_f\data\IGUI\Cfg\holdactions\holdAction_hack_ca.paa',
-
 '_this distance _target < 3',
 '_caller distance _target < 3',
-{},
-{},
-{ []execVM  'ArG_Core\functions\fn_BuzzerBox.sqf' },
-{},
-[],
-2,
-0,
+{},{},{ []execVM  'ArG_Core\functions\fn_BuzzerBox.sqf' },
+{},[],2,0,
 false,
 false
 ] remoteExec ['BIS_fnc_holdActionAdd', [0,2] select isDedicated, this];";
