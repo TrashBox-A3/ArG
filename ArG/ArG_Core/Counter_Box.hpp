7a2fcfcabab1aea8b1a0ef6f812240f3a3006638
Rodeostar42 <rodeosta42@gmail.com>
2018-05-26 08:55:04 +0900

    dwdwdww

--- ArG/ArG_Core/Counter_Box.hpp
+++ ArG/ArG_Core/Counter_Box.hpp
@@ -18,25 +18,15 @@ class  Land_Laptop_unfolded_F;
 {
 class Action
 {
-displayName = "<img image='\ArG_Core\push.paa' size='1' shadow='false' /> <t color=""#FF4500"">Buzzer Switch";
-displayNameDefault = "<img image='\ArG_Core\push.paa' size='3' shadow='false' /><t color=""#FF4500"">Buzzer Switch";
-priority = 3;
-radius = 20;
-position = "camera";
-showWindow = true;
-hideOnUse = 0;
-onlyForPlayer = 1;
-shortcut = "reloadMagazine";
-condition = "true";
-statement = "this say3D 'Switch',[this,'Time Counter',
-'\a3\ui_f\data\IGUI\Cfg\holdactions\holdAction_connect_ca.paa',
-'\a3\ui_f\data\IGUI\Cfg\holdactions\holdAction_hack_ca.paa',
-'_this distance _target < 3',
-'_caller distance _target < 3',
-{},
-{},
-{[]execVM  'ArG_Core\functions\fn_BuzzerBox.sqf'},
-{},[],2,0,true,false] remoteExec ['BIS_fnc_holdActionAdd', [0,2] select isDedicated, this]";
+  "this say3D 'Switch',[this,'Time Counter',
+  '\a3\ui_f\data\IGUI\Cfg\holdactions\holdAction_connect_ca.paa',
+  '\a3\ui_f\data\IGUI\Cfg\holdactions\holdAction_hack_ca.paa',
+  '_this distance _target < 3',
+  '_caller distance _target < 3',
+  {},
+  {},
+  {[]execVM  'ArG_Core\functions\fn_BuzzerBox.sqf'},
+  {},[],2,0,true,false] remoteExec ['BIS_fnc_holdActionAdd', [0,2] select isDedicated, this]";
 };
 };
 
