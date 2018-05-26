dcd741970c0836745d4172e8347b7f61fae7a35a
Rodeostar42 <rodeosta42@gmail.com>
2018-05-26 10:38:26 +0900

    dwdwdwdw

--- ArG/ArG_Core/CfgFunctions.hpp
+++ ArG/ArG_Core/CfgFunctions.hpp
@@ -20,7 +20,7 @@ class ArG
 		class Ai_HitCall {postInit = 1;};
 		class BuzzerBox {};
 		class CountDown {postInit = 1;};
-		class Counter {postInit = 1;};
+		class Counter {};
 		class Hit_drawIcon3D {};
 	};
 
