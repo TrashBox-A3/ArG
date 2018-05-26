f499299dbb672c3a94d03d38486b19e853f8a8c7
Rodeostar42 <rodeosta42@gmail.com>
2018-05-26 09:02:05 +0900

    fefefefe

--- ArG/ArG_Core/Counter_Box.hpp
+++ ArG/ArG_Core/Counter_Box.hpp
@@ -18,22 +18,17 @@ class  Land_Laptop_unfolded_F;
 {
 class Action
 {
-  [this,"Time Counter",
-"\a3\ui_f\data\IGUI\Cfg\holdactions\holdAction_connect_ca.paa",
-"\a3\ui_f\data\IGUI\Cfg\holdactions\holdAction_hack_ca.paa",
-
-"_this distance _target < 3",
-"_caller distance _target < 3",
-{},
-{},
-{ []execVM  "ArG_Core\functions\fn_BuzzerBox.sqf" },
-{},
-[],
-2,
-0,
-false,
-false
-] remoteExec ["BIS_fnc_holdActionAdd", [0,2] select isDedicated, this];
+  displayName = "<img image='\ArG_Core\push.paa' size='1' shadow='false' /> <t color=""#FF4500"">Buzzer Switch";
+  displayNameDefault = "<img image='\ArG_Core\push.paa' size='3' shadow='false' /><t color=""#FF4500"">Buzzer Switch";
+  priority = 3;
+  radius = 20;
+  position = "camera";
+  showWindow = true;
+  hideOnUse = 0;
+  onlyForPlayer = 1;
+  shortcut = "reloadMagazine";
+  condition = "true";
+  statement = "this say3D 'Switch',[[[],'ArG_Core\functions\fn_BuzzerBox.sqf'], 'BIS_fnc_execVM', true,true] call BIS_fnc_MP;";
 };
 };
 
