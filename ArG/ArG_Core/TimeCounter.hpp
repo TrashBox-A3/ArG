018d5fff1ed89044229e8177a23ca3cb46a2808c
Rodeostar42 <rodeosta42@gmail.com>
2018-05-30 07:01:04 +0900

    grgrgrgrggr

--- ArG/ArG_Core/TimeCounter.hpp
+++ ArG/ArG_Core/TimeCounter.hpp
@@ -51,4 +51,40 @@ class  Land_Loudspeakers_F;
     hiddenSelections[]={};
     hiddenSelectionsTextures[] = {};
 
+      class EventHandlers
+    {
+      init = "while {missionNamespace getvariable 'Buzz' == 0} do
+
+      {
+
+
+        {
+            _x setDamage 1;
+            sleep 0.1;
+            _x setDamage 0;
+            sleep 0.1;
+            _x say3D 'Buzzer'
+          } forEach _Speakers;
+          sleep 5;
+       waituntil{missionNamespace getvariable 'Buzz' == 0};
+
+      };
+
+      while {missionNamespace getvariable 'Buzz' == 1} do
+
+      {
+
+
+        {
+           _x setDamage 1;
+           sleep 0.1;
+           _x setDamage 0;
+           sleep 0.1;
+           _x say3D 'Buzzer2'
+         } forEach _Speakers;
+           sleep 11;
+       waituntil{missionNamespace getvariable 'Buzz' == 1};
+      };"
+    };
+
 };
