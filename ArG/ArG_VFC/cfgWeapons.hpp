765a4059df6e990000b8c8c19ad531503d82eeb2
Rodeostar42 <rodeosta42@gmail.com>
2018-05-30 18:48:09 +0900

    dwdwddw

--- ArG/ArG_VFC/cfgWeapons.hpp
+++ ArG/ArG_VFC/cfgWeapons.hpp
@@ -40,6 +40,31 @@ class CfgWeapons
           "VFC_30Rnd_556x45_M4_HK416_025"
         };
 
+        class WeaponSlotsInfo: WeaponSlotsInfo
+        {
+          class MuzzleSlot: MuzzleSlot
+          {
+            linkProxy="\A3\data_f\proxies\weapon_slots\MUZZLE";
+            compatibleItems[]=
+            {
+              "muzzle_snds_h",
+              "ArG_muzzle_snds_H"
+            };
+            iconPosition[]={0,0.44999999};
+            iconScale=0.2;
+          };
+          class CowsSlot: CowsSlot
+          {
+            iconPosition[]={0.5,0.34999999};
+            iconScale=0.2;
+          };
+          class PointerSlot: PointerSlot
+          {
+            iconPosition[]={0.2,0.44999999};
+            iconScale=0.25;
+          };
+        };
+
         initSpeed = 250; // muzzle velocity
         maxRange = 100;
         flash = "";
@@ -234,6 +259,31 @@ class CfgWeapons
 
           "VFC_30Rnd_556x45_M4_HK416_02",
           "VFC_30Rnd_556x45_M4_HK416_025"
+        };
+
+        class WeaponSlotsInfo: WeaponSlotsInfo
+        {
+          class MuzzleSlot: MuzzleSlot
+          {
+            linkProxy="\A3\data_f\proxies\weapon_slots\MUZZLE";
+            compatibleItems[]=
+            {
+              "muzzle_snds_h",
+              "ArG_muzzle_snds_H"
+            };
+            iconPosition[]={0,0.44999999};
+            iconScale=0.2;
+          };
+          class CowsSlot: CowsSlot
+          {
+            iconPosition[]={0.5,0.34999999};
+            iconScale=0.2;
+          };
+          class PointerSlot: PointerSlot
+          {
+            iconPosition[]={0.2,0.44999999};
+            iconScale=0.25;
+          };
         };
 
         initSpeed = 250; // muzzle velocity
