f7e80f8c2d1228767bbbbac0542e0ac2ec8bd278
Rodeostar42 <rodeosta42@gmail.com>
2018-06-01 17:39:53 +0900

    44

--- ArG/ArG_Core/TimeCounter.hpp
+++ ArG/ArG_Core/TimeCounter.hpp
@@ -16,7 +16,22 @@ class  Land_SatellitePhone_F;
 
       class EventHandlers
           {
-               init = "(_this select 0) execVM 'ArG_Core\functions\fn_CounterBox.sqf'";
+               init = "[this,'Time Counter',
+'\a3\ui_f\data\IGUI\Cfg\holdactions\holdAction_connect_ca.paa',
+'\a3\ui_f\data\IGUI\Cfg\holdactions\holdAction_hack_ca.paa',
+
+'_this distance _target < 3',
+'_caller distance _target < 3',
+{},
+{},
+{[]execVM  'ArG_Core\functions\fn_BuzzerBox.sqf'},
+{},
+[],
+2,
+0,
+false,
+false
+] remoteExec ['BIS_fnc_holdActionAdd', [0,2] select isDedicated, this];";
 
           };
 
