735a23e7af560c9e500708c43267e21fc9024286
Rodeostar42 <rodeosta42@gmail.com>
2018-06-01 17:54:35 +0900

    dwdwddw

--- ArG/ArG_Core/TimeCounter.hpp
+++ ArG/ArG_Core/TimeCounter.hpp
@@ -16,22 +16,7 @@ class  Land_SatellitePhone_F;
 
       class EventHandlers
           {
-               init = "[_this,"Time Counter",
-"\a3\ui_f\data\IGUI\Cfg\holdactions\holdAction_connect_ca.paa",
-"\a3\ui_f\data\IGUI\Cfg\holdactions\holdAction_hack_ca.paa",
-
-"_this distance _target < 3",
-"_caller distance _target < 3",
-{},
-{},
-{ []execVM  "ArG_Core\functions\fn_BuzzerBox.sqf" },
-{},
-[],
-2,
-0,
-false,
-false
-] call BIS_fnc_holdActionAdd;";
+               init = "this say3D 'Buzzer2'";
 
           };
 
