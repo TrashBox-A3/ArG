f9a3866b8dff5c594c6fa10324830ae6b0f11999
Rodeostar42 <rodeosta42@gmail.com>
2018-06-02 07:52:02 +0900

    hjjhhjhj

--- ArG/ArG_Core/TimeCounter.hpp
+++ ArG/ArG_Core/TimeCounter.hpp
@@ -16,7 +16,7 @@ class  Land_SatellitePhone_F;
 
       class EventHandlers
           {
-               init = "0=[(_this select 0),'Time Counter','\a3\ui_f\data\IGUI\Cfg\holdactions\holdAction_connect_ca.paa','\a3\ui_f\data\IGUI\Cfg\holdactions\holdAction_hack_ca.paa','_this distance _target < 3','_caller distance _target < 3',{},{missionNamespace setVariable ['Buzz',2,true];systemChat'Count Swaping'},{[]execVM 'ArG_Core\functions\fn_BuzzerBox.sqf'},{systemChat'Count Stop'},[],2,0,false,false] call BIS_fnc_holdActionAdd;";
+               init = "0=[(_this select 0),'Time Counter','\a3\ui_f\data\IGUI\Cfg\holdactions\holdAction_connect_ca.paa','\a3\ui_f\data\IGUI\Cfg\holdactions\holdAction_hack_ca.paa','_this distance _target < 3','_caller distance _target < 3',{},{},{[]execVM 'ArG_Core\functions\fn_BuzzerBox.sqf'},{},[],2,0,false,false] call BIS_fnc_holdActionAdd;";
 
           };
 
