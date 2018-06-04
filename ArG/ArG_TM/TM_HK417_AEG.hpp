067b2c129bb764e36418d3ec8bda747c9e9d8d32
Rodeostar42 <rodeosta42@gmail.com>
2018-06-04 19:24:43 +0900

    feefe

--- ArG/ArG_TM/TM_HK417_AEG.hpp
+++ ArG/ArG_TM/TM_HK417_AEG.hpp
@@ -1,8 +1,9 @@
-SMA_HK417:
-class ArG_TM_HK417_AEG: SMA_HK417
-	{
+///SCAR H (FDE)///
+class SMA_HK417_16in;
 
-    author="Rodeostar42";
+class ArG_TM_HK417_AEG: SMA_HK417_16in
+{
+  author="Rodeostar42";
 
   scope=2;
   displayName="ArG TM HK417 EARLY VARIANT";
@@ -18,24 +19,33 @@ class ArG_TM_HK417_AEG: SMA_HK417
       "ArG_600Rnd_HK417_BB_02",
       "ArG_600Rnd_HK417_BB_025"
   };
-		class WeaponSlotsInfo: WeaponSlotsInfo
-		{
 
-      class MuzzleSlot: MuzzleSlot
-			{
-				linkProxy="\A3\data_f\proxies\weapon_slots\MUZZLE";
-				compatibleItems[]=
-				{
-					"SMA_supptan_762",
-					"SMA_supp_762",
-					"sma_gemtech_one_blk",
-          "ArG_muzzle_snds_H"
-				};
-			};
+  class WeaponSlotsInfo: WeaponSlotsInfo
+  {
+    class MuzzleSlot: MuzzleSlot
+    {
+      linkProxy="\A3\data_f\proxies\weapon_slots\MUZZLE";
+      compatibleItems[]=
+      {
+        "muzzle_snds_h",
+        "ArG_muzzle_snds_H"
+      };
+      iconPosition[]={0,0.44999999};
+      iconScale=0.2;
+    };
+    class CowsSlot: CowsSlot
+    {
+      iconPosition[]={0.5,0.34999999};
+      iconScale=0.2;
+    };
+    class PointerSlot: PointerSlot
+    {
+      iconPosition[]={0.2,0.44999999};
+      iconScale=0.25;
+    };
+  };
 
-		scope=2;
-		inertia=0.81999999;
-    initSpeed = 250; // muzzle velocity
+  initSpeed = 250; // muzzle velocity
   maxRange = 100;
   flash = "gunfire";
   flashSize = 0;
@@ -47,38 +57,20 @@ class ArG_TM_HK417_AEG: SMA_HK417
   maxRecoilSway=0.0125;
   swayDecaySpeed=1.25;
   soundBullet[]={"emptySound",1};
-		handAnim[]=
-		{
-			"OFP2_ManSkeleton",
-			"SMA_HK417\Data\Anim\HK416agf.rtm"
-		};
 
 
-		reloadmagazinesound[]=
-		{
-			"SMA_SOUND\data\HK417\ARRL762.ogg",
-			1,
-			1,
-			35
-		};
-		changeFiremodeSound[]=
-		{
-			"SMA_SOUND\data\HK417\AR1",
-			0.25118899,
-			1,
-			20
-		};
-		modes[]=
-		{
+
+
+  modes[]=
+  {
       "Single",
-      "FullAuto"
-		};
-		opticsZoomMin=0.375;
-		opticsZoomMax=1.1;
-		opticsZoomInit=0.75;
-		discreteDistance[]={100,200,300,400,500,600,700,1000};
-		class Single: Mode_SemiAuto
-    {
+      "FullAuto",
+      "fullauto_medium",
+      "single_medium_optics1",
+      "single_far_optics2"
+  };
+  class Single: Mode_SemiAuto
+  {
       reloadTime=0.096000001;
 
       flash = "";
@@ -105,8 +97,8 @@ class ArG_TM_HK417_AEG: SMA_HK417
       weaponSoundEffect = "DefaultRifle";
   };
   };
-		class FullAuto: Mode_FullAuto
-    {
+  class FullAuto: Mode_FullAuto
+  {
       reloadTime=0.096000001;
       dispersion=0.00086999999;
 
@@ -134,4 +126,95 @@ class ArG_TM_HK417_AEG: SMA_HK417
       weaponSoundEffect = "DefaultRifle";
   };
   };
-	};
+  class fullauto_medium: FullAuto
+  {
+      showToPlayer=0;
+      burst=3;
+      aiBurstTerminable=1;
+      minRange=2;
+      minRangeProbab=0.5;
+      midRange=75;
+      midRangeProbab=0.69999999;
+      maxRange=150;
+      maxRangeProbab=0.050000001;
+      aiRateOfFire=2;
+      aiRateOfFireDistance=200;
+      flash = "";
+      flashSize = 0;
+      fireLightIntensity=0;
+      drySound[] = {"ArG_TM\Sound\M4_SOPMOD.wss",4, 1,15};
+      class StandardSound
+  {
+      begin1[] = {"ArG_TM\Sound\M4_SOPMOD.wss",4, 1,15};
+      soundBegin[] = {"begin1",1};
+      weaponSoundEffect = "DefaultRifle";
+  };
+      class SilencedSound
+  {
+      begin1[] = {"ArG_TM\Sound\M4_SOPMOD.wss",3, 1,15};
+      soundBegin[] = {"begin1",1};
+      weaponSoundEffect = "DefaultRifle";
+  };
+  };
+  class single_medium_optics1: Single
+  {
+      requiredOpticType=1;
+      showToPlayer=0;
+      minRange=2;
+      minRangeProbab=0.2;
+      midRange=450;
+      midRangeProbab=0.69999999;
+      maxRange=600;
+      maxRangeProbab=0.2;
+      aiRateOfFire=6;
+      aiRateOfFireDistance=600;
+      flash = "";
+      flashSize = 0;
+      fireLightIntensity=0;
+      drySound[] = {"ArG_TM\Sound\M4_SOPMOD.wss",4, 1,15};
+      class StandardSound
+  {
+      begin1[] = {"ArG_TM\Sound\M4_SOPMOD.wss",4, 1,15};
+      soundBegin[] = {"begin1",1};
+      weaponSoundEffect = "DefaultRifle";
+  };
+      class SilencedSound
+  {
+      begin1[] = {"ArG_TM\Sound\M4_SOPMOD.wss",3, 1,15};
+      soundBegin[] = {"begin1",1};
+      weaponSoundEffect = "DefaultRifle";
+  };
+  };
+  class single_far_optics2: single_medium_optics1
+  {
+      requiredOpticType=2;
+      showToPlayer=0;
+      minRange=100;
+      minRangeProbab=0.1;
+      midRange=500;
+      midRangeProbab=0.60000002;
+      maxRange=700;
+      maxRangeProbab=0.050000001;
+      aiRateOfFire=8;
+      aiRateOfFireDistance=700;
+      flash = "";
+      flashSize = 0;
+      fireLightIntensity=0;
+      drySound[] = {"ArG_TM\Sound\M4_SOPMOD.wss",4, 1,15};
+      class StandardSound
+  {
+      begin1[] = {"ArG_TM\Sound\M4_SOPMOD.wss",4, 1,15};
+      soundBegin[] = {"begin1",1};
+      weaponSoundEffect = "DefaultRifle";
+  };
+      class SilencedSound
+  {
+      begin1[] = {"ArG_TM\Sound\M4_SOPMOD.wss",3, 1,15};
+      soundBegin[] = {"begin1",1};
+      weaponSoundEffect = "DefaultRifle";
+  };
+  };
+
+
+
+};
