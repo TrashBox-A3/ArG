7ed879c297a0d6f59c9ca577f86f9d952e8c12aa
Rodeostar42 <rodeosta42@gmail.com>
2018-06-04 18:43:49 +0900

    dwdwdwdw

--- ArG/ArG_TM/TM_HK417_AEG.hpp
+++ ArG/ArG_TM/TM_HK417_AEG.hpp
@@ -44,12 +44,12 @@ class CfgWeapons
 	class ArG_TM_HK417_AEG: SMA_762_RIFLEBASE
 	{
 		scope=2;
-		dlc="SMA";
+
 		inertia=0.80000001;
 		dexterity=1.6;
 		recoil="recoil_dmr_06";
-		displayName="HK417 Assaulter 12in";
-		author="Specialist Military Arms";
+		displayName="HK417 ArG";
+		author="Rodeostar42";
 		handAnim[]=
 		{
 			"OFP2_ManSkeleton",
@@ -196,139 +196,5 @@ class CfgWeapons
 			};
 		};
 	};
-	class SMA_HK417vfg: SMA_HK417
-	{
-		class WeaponSlotsInfo: WeaponSlotsInfo
-		{
-			mass=83.199997;
-			class PointerSlot: PointerSlot
-			{
-				compatibleItems[]=
-				{
-					"SMA_ANPEQ15_TOP_417TAN",
-					"SMA_ANPEQ15_TOP_417BLK",
-					"SMA_SFPEQ_HK417TOP_TAN",
-					"SMA_SFPEQ_HK417TOP_TAN_LIGHT",
-					"SMA_SFPEQ_HK417TOP_BLK",
-					"SMA_SFPEQ_HK417TOP_BLK_LIGHT"
-				};
-			};
-			class asdg_Underbarrel
-			{
-				access=0;
-				iconPicture="\A3\Weapons_F_Mark\Data\UI\attachment_under.paa";
-				iconPinpoint="Bottom";
-				iconPosition[]={0,0};
-				iconScale=0;
-				linkProxy="\A3\Data_F_Mark\Proxies\Weapon_Slots\UNDERBARREL";
-				scope=0;
-				class CompatibleItems
-				{
-					SMA_Gripod_01=1;
-				};
-			};
-		};
-		scope=2;
-		displayName="HK417 Assaulter 12in(vfg)";
-		author="Specialist Military Arms";
-		handAnim[]=
-		{
-			"OFP2_ManSkeleton",
-			"SMA_HK417\Data\Anim\HK416vfg.rtm"
-		};
-		model="\SMA_HK417\HK417vfg.p3d";
-		picture="\SMA_HK417\ui\SMAHK417_ca.paa";
-		descriptionShort="7.62mm Battle Rifle";
-		reloadaction="GestureReloadSMG_02";
-		reloadmagazinesound[]=
-		{
-			"SMA_SOUND\data\HK417\ARRL762.ogg",
-			1,
-			1,
-			35
-		};
-		changeFiremodeSound[]=
-		{
-			"SMA_SOUND\data\HK417\AR1",
-			0.25118899,
-			1,
-			20
-		};
-		modes[]=
-		{
-			"Single",
-			"FullAuto"
-		};
-		opticsZoomMin=0.375;
-		opticsZoomMax=1.1;
-		opticsZoomInit=0.75;
-		discreteDistance[]={100,200,300,400,500};
-	};
-	class SMA_HK417_16in: SMA_HK417
-	{
-		class WeaponSlotsInfo: WeaponSlotsInfo
-		{
-			class asdg_UnderSlot_SMAAssault: asdg_UnderSlot
-			{
-			};
-			mass=88;
-			class asdg_FrontSideRail_SMAAssault: asdg_FrontSideRail
-			{
-				class compatibleItems: compatibleItems
-				{
-					SMA_ANPEQ15_TOP_417TAN=1;
-					SMA_ANPEQ15_TOP_417BLK=1;
-					SMA_SFPEQ_HK417TOP_TAN=1;
-					SMA_SFPEQ_HK417TOP_TAN_LIGHT=1;
-					SMA_SFPEQ_HK417TOP_BLK=1;
-					SMA_SFPEQ_HK417TOP_BLK_LIGHT=1;
-				};
-			};
-		};
-		scope=2;
-		inertia=0.81999999;
-		recoil="recoil_dmr_06";
-		displayName="HK417 Recce 16in ";
-		author="Specialist Military Arms";
-		handAnim[]=
-		{
-			"OFP2_ManSkeleton",
-			"SMA_HK417\Data\Anim\HK416agf.rtm"
-		};
-		model="\SMA_HK417\HK417_16in.p3d";
-		picture="\SMA_HK417\ui\SMAHK41716in_ca.paa";
-		descriptionShort="7.62mm Battle Rifle";
-		reloadaction="GestureReloadSMG_02";
-		reloadmagazinesound[]=
-		{
-			"SMA_SOUND\data\HK417\ARRL762.ogg",
-			1,
-			1,
-			35
-		};
-		changeFiremodeSound[]=
-		{
-			"SMA_SOUND\data\HK417\AR1",
-			0.25118899,
-			1,
-			20
-		};
-		modes[]=
-		{
-			"Single",
-			"FullAuto"
-		};
-		opticsZoomMin=0.375;
-		opticsZoomMax=1.1;
-		opticsZoomInit=0.75;
-		discreteDistance[]={100,200,300,400,500,600,700,1000};
-		class Single: Single
-		{
-			dispersion=0.00028581001;
-		};
-		class FullAuto: FullAuto
-		{
-			dispersion=0.00028581001;
-		};
-	};
+
 };
