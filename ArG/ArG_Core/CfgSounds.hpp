18448d9b2a3ada5a63d2c1c1785c148cff94668e
Rodeostar42 <rodeosta42@gmail.com>
2018-06-05 18:05:20 +0900

    dwdwdwdwdwdwdw

--- ArG/ArG_Core/CfgSounds.hpp
+++ ArG/ArG_Core/CfgSounds.hpp
@@ -2,43 +2,43 @@ class CfgSounds {
 
   class HitCall {
     name = "";
-    sound[] = {"ArG_Core\Sound\HitCall.wss", 7, 1.1};
+    sound[] = {"ArG_Core\Sound\HitCall.wss", 10, 1.1};
     titles[] = {};
   };
 
   class Buzzer {
     name = "";
-    sound[] = {"ArG_Core\Sound\Buzzer.wss", 3, 1.1};
+    sound[] = {"ArG_Core\Sound\Buzzer.wss", 50, 1.1};
     titles[] = {};
   };
 
   class Buzzer2 {
     name = "";
-    sound[] = {"ArG_Core\Sound\Buzzer_02.wss", 5, 1.1};
+    sound[] = {"ArG_Core\Sound\Buzzer_02.wss", 50, 1.1};
     titles[] = {};
   };
 
   class Switch {
     name = "";
-    sound[] = {"ArG_Core\Sound\Switch.wss", 1, 1.1};
+    sound[] = {"ArG_Core\Sound\Switch.wss", 3, 1.1};
     titles[] = {};
   };
 
   class Click {
     name = "";
-    sound[] = {"ArG_Core\Sound\Click.wss", 1, 1.1};
+    sound[] = {"ArG_Core\Sound\Click.wss", 3, 1.1};
     titles[] = {};
   };
 
   class Call_Start {
     name = "";
-    sound[] = {"ArG_Core\Sound\Call_Start.wss", 1, 1.1};
+    sound[] = {"ArG_Core\Sound\Call_Start.wss", 3, 1.1};
     titles[] = {};
   };
 
   class Call_Tel {
     name = "";
-    sound[] = {"ArG_Core\Sound\Call_Tel.wss", 1, 1.1};
+    sound[] = {"ArG_Core\Sound\Call_Tel.wss", 3, 1.1};
     titles[] = {};
   };
 
--- ArG/ArG_Core/functions/fn_Muzzle.sqf
+++ ArG/ArG_Core/functions/fn_Muzzle.sqf
@@ -20,11 +20,52 @@ _ArGGamer = [
 "ArG_Gamer_12"
 ];
 
+_ArGWeapons = [
+"ArG_AK_PKM",
+"ArG_AK_SR25K",
+"ArG_MX_F",
+"ArG_MX_F_BLK",
+"ArG_Katiba_F",
+"ArG_Mk20_F",
+"ArG_EBR_F",
+"ArG_Mk200_F",
+"ArG_GHK_AKM",
+"ArG_KSC_PM",
+"ArG_KSC_AK74M",
+"ArG_LCT_AKM",
+"ArG_LCT_VAL",
+"ArG_TM_HK416_DEV",
+"ArG_TM_M4A1_NG",
+"ArG_TM_M4A1_NWS",
+"ArG_TM_SCAR_L_TAN",
+"ArG_TM_SCAR_H_TAN",
+"ArG_TM_Glock17",
+"ArG_TM_P226R",
+"ArG_TM_M92F",
+"ArG_M320",
+"ArG_TM_AK47T3",
+"ArG_TM_VSR10_G",
+"ArG_TM_HK417_AEG",
+"ArG_VFC_HK416A5_GBB_BLK",
+"ArG_VFC_HK416A5_GBB_SND",
+"ArG_VFC_M27IAR_GBB",
+"ArG_VFC_MP7A1_GBB",
+"ArG_WE_SVD"
+];
+
 while{!(player hasweapon "")}do{
 
 {
   if ((isPlayer _x)&&(typeOf _x in _ArGGamer)) then {
 
+    if((weapons player in _ArGWeapons))then{
+         if(!(handgunWeapon player in _ArGWeapons))then{
+        player removeWeapon (handgunWeapon player);
+       };
+           if(!(primaryWeapon player in _ArGWeapons))then{
+             player removeWeapon (primaryWeapon player);
+            };
+          };
     if(!(primaryWeapon player isEqualTo "")&&(primaryWeaponItems _x select 0 == ""))then{
 
     player addPrimaryWeaponItem "ArG_muzzle_snds_H";
